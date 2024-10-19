3872cc9633a861264d855bbe55a4c13b1f9cc242
josh233 <josh233@users.noreply.github.com>
2024-10-19 16:28:42 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -509,7 +509,7 @@ export const CLASH_CONFIG = {
 		'default-nameserver': ['https://223.5.5.5/dns-query'],
 		nameserver: ['https://dns.alidns.com/dns-query', 'https://doh.pub/dns-query'],
 		fallback: ["'https://one.one.one.one/dns-query#🚀 节点选择'"],
-		'fallback-filter': [{'geoip': true, 'geoip-code': 'CN'}],
+		'fallback-filter': ["geoip: true", "'geoip-code': 'CN'"],
 		'proxy-server-nameserver':['https://dns.alidns.com/dns-query', 'https://doh.pub/dns-query']
 	},
 	proxies: [],

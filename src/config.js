e358a8099f3c35446f443b75f689ca8e13d28870
josh233 <josh233@users.noreply.github.com>
2024-10-19 12:39:21 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -458,7 +458,7 @@ export const SING_BOX_CONFIG = {
 	],
 	outbounds: [
 		{ type: 'direct', tag: 'DIRECT' },
-		{ type: 'block', tag: 'block' },
+		{ type: 'block', tag: 'REJECT' },
 		{ type: 'dns', tag: 'dns-out' }
 	],
 	route : {

76ba248a494ccba1b24c98e15e21c5b00a24b75e
josh233 <josh233@users.noreply.github.com>
2024-10-21 08:59:00 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -497,13 +497,13 @@ export const CLASH_CONFIG = {
 		'override-destination': true,
 		sniff: {
 		  QUIC: {
-		     ports: \[ 443 \]
+		     ports: '[ 443 ]'
 		  },
                   TLS: {
-		     ports: \[443, 8443\]
+		     ports: '[443, 8443]'
 		  },
                   HTTP: {
-		     ports: \[80\, 8080-8880\]
+		     ports: '[80, 8080-8880]'
 		  }
                }
 	},

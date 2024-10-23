6798da9e9622b726212d48342c02aac6eecefd34
josh233 <josh233@users.noreply.github.com>
2024-10-23 09:58:35 +0000

    Update htmlBuilder.js

--- src/htmlBuilder.js
+++ src/htmlBuilder.js
@@ -690,7 +690,8 @@ const generateRuleSetSelection = () => `
     </div>
 
     <div class="content-container mb-3">
-      <label for="predefinedRules" class="form-label">Rule Sets: (私有局域网、常见国内网站、常见非中国网站列表是必选项)</label>
+      <label for="predefinedRules" class="form-label">Rule Sets:</label>
+      <p> (私有局域网、常见国内网站、常见非中国网站列表是必选项)</p>
       <select class="form-select" id="predefinedRules" onchange="applyPredefinedRules()">
         <option value="custom">Custom</option>
         <option value="minimal">Minimal</option>

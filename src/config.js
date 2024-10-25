2dc55318958f4947a94e79e47e5f5fba7edc63ef
josh233 <josh233@users.noreply.github.com>
2024-10-25 04:20:45 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -571,7 +571,20 @@ export const SING_BOX_CONFIG = {
 	outbounds: [
 		{ type: 'direct', tag: 'DIRECT' },
 		{ type: 'block', tag: 'REJECT' },
-		{ type: 'dns', tag: 'dns-out' }
+		{ type: 'dns', tag: 'dns-out' },
+		{
+			"type": "logical",
+			"mode": "and",
+			"rules": [
+			    {
+				"protocol": "quic"
+			    },
+			    {
+				"rule_set": "geolocation-!cn"
+			    }
+			],
+			"outbound": "REJECT"
+               },
 	],
 	route : {
 	},
